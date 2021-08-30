664e108ca64b474d17ec7780b718e380a29dfc8b
silvana <silvana.belen.garcia@gmail.com>
2021-08-30 16:04:43 -0300

    New clock implemented with a class, using setInterval and setState()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import ClockClass from './components/ClockClass'
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.App}>
-      <h1>Clock</h1>
-      <p className={styles.pClock}>{new Date().toLocaleTimeString()}</p>
+      <ClockClass/>
     </div>
   );
 }
